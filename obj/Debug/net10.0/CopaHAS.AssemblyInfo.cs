64c22942e325032382b4c035eee1aacfeffb544b
Larissa <lariamaral5566@gmail.com>
2026-05-04 09:06:38 -0300

    seila

--- obj/Debug/net10.0/CopaHAS.AssemblyInfo.cs
+++ obj/Debug/net10.0/CopaHAS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CopaHAS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a0ae20bcd8304ab576cc7a66e24114928798ac3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67682e66330969ccab988533d674a7b42710e722")]
 [assembly: System.Reflection.AssemblyProductAttribute("CopaHAS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CopaHAS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
